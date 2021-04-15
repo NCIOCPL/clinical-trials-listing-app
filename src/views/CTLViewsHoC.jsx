3507049cbd4f79838d4a4e796b5ff4c6ca8f6bb5
Sarina <sarinapadilla@users.noreply.github.com>
2021-04-15 11:11:29 -0400

    Initial refactor pass of HoC

--- src/views/CTLViewsHoC.jsx
+++ src/views/CTLViewsHoC.jsx
@@ -5,7 +5,7 @@ import { useNavigate, useLocation, useParams } from 'react-router';
 import { Spinner } from '../components';
 import NoTrialsFound from './NoTrialsFound';
 import { queryParamType } from '../constants';
-import { useAppPaths, fetchAllTheThings } from '../hooks';
+import { useAppPaths, useCustomQuery } from '../hooks';
 
 import {
 	appendOrUpdateToQueryString,
@@ -13,50 +13,50 @@ import {
 	matchQueryParam,
 } from '../utils';
 
-// import {
-// 	getListingInformationById,
-// 	getListingInformationByName,
-// } from '../services/api/actions';
+import {
+	getListingInformationById,
+	getListingInformationByName,
+	getTrialType,
+} from '../services/api/actions';
 
 const CTLViewsHoC = (WrappedView) => {
 	const WithPreFetch = (props) => {
 		const { CodeOrPurlPath } = useAppPaths();
-		const { codeOrPurl } = useParams();
+		const { codeOrPurl, codeOrPurlWithType } = useParams();
 		const location = useLocation();
 		const navigate = useNavigate();
 		const { search } = location;
+
 		const isNoTrials = codeOrPurl.startsWith('notrials');
 		const listingPattern = WrappedView?.name;
 
+		const [codeOrPurlToFetch, setCodeOrPurlToFetch] = useState(null);
+		const [trialTypeToFetch, setTrialTypeToFetch] = useState(null);
 		const [showNoTrialsFound, setShowNoTrialsFound] = useState(false);
 		const [prerenderStatusCode, setPrerenderStatusCode] = useState(null);
-		//const [shouldFetchListingInfo, setShouldFetchListingInfo] = useState(false);
+		const [shouldFetchData, setShouldFetchData] = useState(false);
 		const [stateListingInfo, setStateListingInfo] = useState(null);
 		const [doneLoading, setDoneLoading] = useState(false);
 		const [hasBeenRedirected, setHasBeenRedirected] = useState(false);
-		const [paramType, setParamType] = useState(queryParamType.code);
-		const [queryParam, setQueryParam] = useState();
 
-		const getFetchByIdOrName =
-			paramType === queryParamType.code
-				? 'getListingInformationById'
-				: 'getListingInformationByName';
+		const getFetchByIdOrName = (paramType) => {
+			return (
+				paramType === queryParamType.code
+					? getListingInformationById
+					: getListingInformationByName
+			);
+		}
 
-		const paramToFetch = {
-			fetchName: getFetchByIdOrName,
-			fetchParams: queryParam,
-		};
-
-		// const getListingInfo = useCustomQuery(
-		// 	getFetchByIdOrName({ queryParam }),
-		// 	shouldFetchListingInfo && !hasBeenRedirected
-		// );
-		const getListingInfo = fetchAllTheThings(paramToFetch);
+		const getListingInfo = useCustomQuery(
+			getFetchByIdOrName({ queryParam }),
+			shouldFetchListingInfo && !hasBeenRedirected
+		);
 
 		useEffect(() => {
 			// If our current route is '/notrials', we have different logic
 			// for handling the No Trials Found page.
 			if (isNoTrials) {
+				setShowNoTrialsFound(true);
 				// If we have the listing information in the state,
 				// we have been redirected here.
 				if (
@@ -64,43 +64,61 @@ const CTLViewsHoC = (WrappedView) => {
 					location.state?.listingInfo &&
 					location.state?.redirectStatus
 				) {
-					setShowNoTrialsFound(true);
 					setPrerenderStatusCode(location.state.redirectStatus);
 					setStateListingInfo(location.state.listingInfo);
 				} else {
 					// We have come directly to '/notrials' and we need to look
 					// up the listing information for the given param.
-					setShowNoTrialsFound(true);
 					setPrerenderStatusCode('404');
 
 					const p1 = getKeyValueFromQueryString('p1', search);
 					setFetchByIdOrName(p1);
-					//setShouldFetchListingInfo(true);
+					setShouldFetchData(true);
 				}
 			} else if (!isNoTrials) {
-				// If we are a code or pretty URL name, do the lookup to handle
-				// the Disease page.
-
 				// If we've been redirected to a pretty URL, set this status code.
 				// This turns on the prerender status stuff for Helmet
 				if (location.state?.redirectStatus) {
 					setPrerenderStatusCode(location.state.redirectStatus);
 				}
 
-				setFetchByIdOrName(codeOrPurl);
-				//setShouldFetchListingInfo(true);
+				const param = matchQueryParam(codeOrPurl);
+				const fetchQueryName = getFetchByIdOrName(param.paramType);
+
+				const paramToFetch = {
+					queryName: fetchQueryName,
+					param: codeOrPurl
+				}
+
+				setCodeOrPurlToFetch(paramToFetch);
+
+				if (codeOrPurlWithType) {
+					const trialTypeToFetch = {
+						queryName: getTrialType,
+						param: CodeOrPurlWithTypePath,
+					}
+					setTrialTypeToFetch(trialTypeToFetch);
+				}
+
+				setShouldFetchData(true);
 			}
 		}, [codeOrPurl]);
 
 		useEffect(() => {
+			if (codeOrPurlToFetch) {
+
+			}
+		}, [codeOrPurlToFetch, trialTypeToFetch, shouldFetchData]);
+
+		useEffect(() => {
 			if (
 				!getListingInfo.loading &&
-				getListingInfo.payload[0] &&
+				getListingInfo.payload &&
 				!hasBeenRedirected
 			) {
-				const { prettyUrlName } = getListingInfo.payload[0];
+				const { prettyUrlName } = getListingInfo.payload;
 
-				setStateListingInfo(getListingInfo.payload[0]);
+				setStateListingInfo(getListingInfo.payload);
 
 				// Redirect to pretty url if one exists for listing info
 				if (prettyUrlName && paramType === 'code' && !isNoTrials) {
