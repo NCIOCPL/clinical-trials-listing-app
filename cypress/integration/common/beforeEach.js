54483d16bac890462a507b51d4257b94c5966a15
Sarina <sarinapadilla@users.noreply.github.com>
2021-04-24 05:49:09 -1000

    (#62) Remove dictionary popup from Intro Text Closes #62
    * Update introText in index.html
    * Update introText in beforeEach.js
    * Update integration tests

--- cypress/integration/common/beforeEach.js
+++ cypress/integration/common/beforeEach.js
@@ -31,7 +31,7 @@ beforeEach(() => {
 			detailedViewPagePrettyUrlFormatter:
 				'/clinicaltrials/{{nci_id}}',
 				introText:
-				'<p>Clinical trials are research studies that involve people. The clinical trials on this list are for breast cancer. All trials on the list are <a class="definition" href="/Common/PopUps/popDefinition.aspx?id=CDR0000776051&amp;version=Patient&amp;language=English" onclick="javascript:popWindow("defbyid","CDR0000776051&amp;version=Patient&amp;language=English"); return false;">supported by NCI</a>.</p><p>NCI’s <a href="/about-cancer/treatment/clinical-trials/what-are-trials">basic information about clinical trials</a> explains the types and phases of trials and how they are carried out. Clinical trials look at new ways to prevent, detect, or treat disease. You may want to think about taking part in a clinical trial. Talk to your doctor for help in deciding if one is right for you.</p>',
+				'<p>Clinical trials are research studies that involve people. The clinical trials on this list are for breast cancer. All trials on the list are NCI-supported clinical trials, which are sponsored or otherwise financially supported by NCI.</p><p>NCI’s <a href="/about-cancer/treatment/clinical-trials/what-are-trials">basic information about clinical trials</a> explains the types and phases of trials and how they are carried out. Clinical trials look at new ways to prevent, detect, or treat disease. You may want to think about taking part in a clinical trial. Talk to your doctor for help in deciding if one is right for you.</p>',
 			trialsApiEndpoint: '/api/',
 		};
 		console.log(win.INT_TEST_APP_PARAMS);
