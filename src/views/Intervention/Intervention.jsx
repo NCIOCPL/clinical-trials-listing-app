4a08457ab7d0d2a6296ff148881744c6fc0db0d0
Rosado <rosadoaa@nih.gov>
2021-05-20 15:45:27 -0400

    (#103) No Trial page with CIS Banner when on-existent page number is appended to url
    * edited Invervetion.jsx and Disease.jsx redirect conditional to use payload.trials.length

--- src/views/Intervention/Intervention.jsx
+++ src/views/Intervention/Intervention.jsx
@@ -123,7 +123,7 @@ const Intervention = ({ routeParamMap, routePath, data }) => {
 
 	useEffect(() => {
 		if (!queryResponse.loading && queryResponse.payload) {
-			if (queryResponse.payload.total === 0) {
+			if (queryResponse.payload.trials.length === 0) {
 				const redirectStatusCode = location.state?.redirectStatus
 					? location.state?.redirectStatus
 					: '302';
