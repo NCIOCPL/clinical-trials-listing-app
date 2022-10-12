026004b25a53493da527675bca2da04b45380578
Alejandro Rosado <rosadoaa@nih.gov>
2022-10-12 14:56:22 -0400

    (#145) Bump Cypress Version to 10.9.0
    - updated Cypress version to 10.9.0
    - ran the migration helper
    - added "stepDefinitions": "cypress/e2e" to package.json
    - updated @cypress/code-coverage to 3.10.0
    - updated cypress-cucumber-preprocessor to 4.3.1

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  video: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: 'cypress/e2e/**/*.feature',
+    baseUrl: 'http://localhost:3000',
+  },
+})
