536aa389ae04c0ad87f586ab083af22588fc6733
Bela Olson <bela.olson@nih.gov>
2024-03-06 14:28:48 -0500

    (#198) Update Node, Webpack, and other security updates
    nvm use 20
    npm audit fix --force
    npm run lint:fix
    updated axios

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,5 +1,4 @@
 import { act, render } from '@testing-library/react';
-import axios from 'axios';
 import nock from 'nock';
 import React from 'react';
 import { MemoryRouter, useLocation } from 'react-router';
@@ -10,8 +9,6 @@ import { MockAnalyticsProvider } from '../tracking';
 import Manual from '../views/Manual';
 
 jest.mock('../store/store');
-
-axios.defaults.adapter = require('axios/lib/adapters/http');
 
 describe('App component', () => {
 	let location;
