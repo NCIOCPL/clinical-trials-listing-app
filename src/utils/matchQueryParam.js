c173d0d7945965fbec77872ea3400c08134288ac
adriancofie <38888889+adriancofie@users.noreply.github.com>
2025-11-03 11:00:44 -0500

    (#225) "carbon-c-14-ac0010" intervention treated as a ccode.
    Updates the regex to start from the beginnning.
    
    Closes #225

--- src/utils/matchQueryParam.js
+++ src/utils/matchQueryParam.js
@@ -6,7 +6,7 @@ import { queryParamType } from '../constants';
  * @param {string} codeOrPurl the code or pretty URL name to match to regex
  */
 export const matchQueryParam = (codeOrPurl) => {
-	const regex = /(C[0-9]+)(,C[0-9]+)*$/i;
+	const regex = /^(C[0-9]+)(,C[0-9]+)*$/i;
 	if (regex.test(codeOrPurl)) {
 		return {
 			paramType: queryParamType.code,
